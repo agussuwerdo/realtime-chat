4c85063fc20c59f6194c853bd610ba7612936edd
Agus Suwerdo <agussuwerdo@gmail.com>
2024-09-07 20:09:20 +0700

    add client src

---
+++ client/src/index.js
@@ -0,0 +1,7 @@
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './tailwind.css'; // Import Tailwind CSS here
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
